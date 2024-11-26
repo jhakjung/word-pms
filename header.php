f9660d3c397c6c97e3112118dbc60ae024cd83ff
JAX <nahak@naver.com>
2024-11-26 09:52:13 +0900

    중간저장

--- header.php
+++ header.php
@@ -7,6 +7,10 @@
   	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="robots" content="noindex, nofollow">
   	<title><?php wp_title(' | ', 'echo', 'right'); ?><?php bloginfo('name'); ?></title>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Song+Myung:400" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Black+Han+Sans:400" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Gothic+A1:100" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 
   	<?php wp_head(); ?>
@@ -17,7 +21,7 @@
     <nav class="site-header mb-2">
         <div class="container d-flex flex-sm-wrap justify-content-between align-content-center py-3">
             <div class="w-auto float-left">
-                <a class="fs-3 text-dark" href="<?php echo esc_url(site_url('/')); ?>"><?php bloginfo('name'); ?></a>
+                <a class="site-name fs-3 text-dark" href="<?php echo esc_url(site_url('/')); ?>"><?php bloginfo('name'); ?></a>
             </div>
 
             <div class="mx-auto w-auto custom-search w-50">
